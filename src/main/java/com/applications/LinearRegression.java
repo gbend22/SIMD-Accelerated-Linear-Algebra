08567321aad3f0929df620be9b345b482220f21e
gbend22 <gbend22@freeuni.edu.ge>
2026-07-09 13:14:52 +0400

    added LinearRegression skeleton with normal-equations fit

---
+++ src/main/java/com/applications/LinearRegression.java
@@ -0,0 +1,67 @@
+package com.applications;
+
+import com.matrix.MatrixOps;
+
+import java.util.Arrays;
+
+public class LinearRegression {
+
+    private final boolean fitIntercept;
+
+    private float[] coefficients;
+    private float intercept;
+    private boolean fitted;
+
+    public LinearRegression() {
+        this(true);
+    }
+
+    public LinearRegression(boolean fitIntercept) {
+        this.fitIntercept = fitIntercept;
+    }
+
+    public void fit(float[][] x, float[] y) {
+        if (x.length == 0) {
+            throw new IllegalArgumentException("Training set must not be empty");
+        }
+        if (x.length != y.length) {
+            throw new IllegalArgumentException(
+                    "Sample count mismatch: " + x.length + " rows but " + y.length + " targets");
+        }
+
+        int features = x[0].length;
+        for (float[] row : x) {
+            if (row.length != features) {
+                throw new IllegalArgumentException("All samples must have the same number of features");
+            }
+        }
+
+        float[][] design = fitIntercept ? augment(x) : x;
+
+        float[][] dt = MatrixOps.transpose(design);
+        float[][] normal = MatrixOps.multiply(dt, design);
+        float[] rhs = MatrixOps.multiply(dt, y);
+
+        float[] beta = MatrixOps.solve(normal, rhs);
+
+        if (fitIntercept) {
+            intercept = beta[0];
+            coefficients = Arrays.copyOfRange(beta, 1, beta.length);
+        } else {
+            intercept = 0f;
+            coefficients = beta;
+        }
+        fitted = true;
+    }
+
+    private static float[][] augment(float[][] x) {
+        int n = x.length;
+        int features = x[0].length;
+        float[][] design = new float[n][features + 1];
+        for (int i = 0; i < n; i++) {
+            design[i][0] = 1f;
+            System.arraycopy(x[i], 0, design[i], 1, features);
+        }
+        return design;
+    }
+}
